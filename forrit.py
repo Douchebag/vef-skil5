65df4c7a9536d47199940dc31ce29efb0fdd68a6
Douchebag <yesimdingo@gmail.com>
2018-02-14 09:15:30 +0000

    error

--- forrit.py
+++ forrit.py
@@ -4,13 +4,17 @@ import requests, os
 response = requests.get('http://apis.is/concerts')
 
 data = response.json()
-print(data['results'][0]['eventHallName'])
 
 @route('/')
 def index():
     return template('index', data=data)
+
 @route("/static/<filename>")
 def server_static(filename):
     return static_file(filename, root='./css')
+
+@error(404)
+def error404(error):
+    return "<h1>Þessi síða fannst ekki</h1>"
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
